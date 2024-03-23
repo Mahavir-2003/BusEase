13824a678ffe980138168664de640d0937f12871
Mahavir-2003 <patelmahavir2003@outlook.com>
2024-03-23 09:22:32 +0530

    Added Date Interface.

---
+++ lib/models/interfaces/date.dart
@@ -0,0 +1,13 @@
+class Date {
+  final int day;
+  final bool isUpCompleted;
+  final bool isDownCompleted;
+  final bool isHoliday;
+
+  Date({
+    required this.day,
+    required this.isUpCompleted,
+    required this.isDownCompleted,
+    required this.isHoliday,
+  });
+}
