b76c61fc33933ea31845e2bd8ab9b3e34711763b
Mahavir-2003 <patelmahavir2003@outlook.com>
2024-03-25 13:13:52 +0530

    Add dynamic greeting message based on time of day and also added dynamic username

--- lib/components/greeting.dart
+++ lib/components/greeting.dart
@@ -1,18 +1,33 @@
-
+import 'package:bus_ease/providers/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Greeting extends StatelessWidget {
   const Greeting({Key? key}) : super(key: key);
+
+  String getGreeting() {
+    final hour = DateTime.now().hour;
+    if (hour < 12) {
+      return 'Good Morning';
+    }
+    if (hour < 17) {
+      return 'Good Afternoon';
+    }
+    return 'Good Evening';
+  }
+
   @override
   Widget build(BuildContext context) {
+    String greet = getGreeting();
+
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
-      child: const Column(
+      child:  Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
            Row(
             children: [
-              Text(
+             const Text(
                 'Hello, ',
                 style: TextStyle(
                   letterSpacing: 0.8,
@@ -22,8 +37,8 @@ class Greeting extends StatelessWidget {
                 ),
               ),
               Text(
-                'Mahavir Patel',
-                style: TextStyle(
+                '${Provider.of<UserProvider>(context, listen: false).user!.firstName} ${Provider.of<UserProvider>(context, listen: false).user!.lastName}',
+                style: const TextStyle(
                   letterSpacing: 0.8,
                   color: Color(0xffFBC420),
                   fontSize: 16,
@@ -33,8 +48,8 @@ class Greeting extends StatelessWidget {
             ],
            ),
            Text(
-            'Good Morning!',
-            style: TextStyle(
+            '$greet!',
+            style: const TextStyle(
               letterSpacing: 0.8,
               color: Colors.white,
               fontSize: 30,
