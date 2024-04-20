6b76d2fc264aeda711e2b155d7bdf5b648f0bb35
Mahavir-2003 <patelmahavir2003@outlook.com>
2024-04-20 09:35:32 +0530

    Created DatabaseModel for Tickets

---
+++ src/models/Ticket/ticket.ts
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+// ticket should automatically destroyed in 24 hours
+const ticketSchema = new mongoose.Schema({
+  userID : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'User',
+    required : true
+  },
+  paasId : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'Paas',
+    required : true
+  },
+  passengerName : {
+    type : String,
+    required : true
+  },
+  Date:{
+    type: Date,
+    default: Date.now,
+  },
+  Depot :{
+    type : String,
+    required : true
+  },
+  type :{
+    type : String,
+    required : true
+  },
+  from : {
+    type : String,
+    required : true
+  },
+    to : {
+        type : String,
+        required : true
+    },
+    status : {
+        type : Boolean,
+        required : true
+    },
+    ticketQuantity : {
+        type : Number,
+        required : true
+    },
+    ticketPrice : {
+        type : Number,
+        required : true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 86400 
+    }
+});
+
+const Ticket = mongoose.model('Ticket', ticketSchema);
+
+export default Ticket;
