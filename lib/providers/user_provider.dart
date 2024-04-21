4b7a5ff3a9efbf5964de048cc241e212611ef95b
Mahavir-2003 <patelmahavir2003@outlook.com>
2024-04-21 17:03:37 +0530

    Add Refresh Token Functionality.

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:bus_ease/models/user_data.dart';
+import 'package:bus_ease/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import "package:http/http.dart" as http;
@@ -27,9 +28,27 @@ class UserProvider extends ChangeNotifier {
         var res = jsonDecode(response.body);
         _user = UserData.fromJson(res);
         notifyListeners();
+      } else if (response.statusCode == 403) {
+        AuthService authService = AuthService(userProvider: this);
+        await authService.refreshToken();
+
+        // Fetch the user again
+        accessToken = await _storage.read(key: "access_token");
+        response = await http.get(url, headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer $accessToken"
+        });
+
+        if (response.statusCode == 200) {
+          var res = jsonDecode(response.body);
+          _user = UserData.fromJson(res);
+          notifyListeners();
+        } else {
+          throw Exception('Failed to load user data after refreshing token');
+        }
       }
     } catch (e) {
-    throw Exception('Failed to load user data');
-}
+      throw Exception('Failed to load user data');
+    }
   }
 }
