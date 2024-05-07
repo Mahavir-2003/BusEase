6d6dff07fdd2942567408a763210cdb8add1e813
Mahavir-2003 <patelmahavir2003@outlook.com>
2024-05-07 17:15:29 +0530

    App Complete the UI rendering Issue

--- lib/screens/ticket/ticket_create.dart
+++ lib/screens/ticket/ticket_create.dart
@@ -314,192 +314,191 @@ class _TicketCreateState extends State<TicketCreate> {
                 style: TextStyle(color: Colors.white),
               ),
             ),
-            body: Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 30.0, vertical: 60.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                      child: Text(
-                        widget.bus_number,
-                        style: const TextStyle(
-                            color: Color.fromARGB(200, 255, 255, 255),
-                            fontSize: 30),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    Text(
-                      'Hello, ${Provider.of<UserProvider>(context).user!.firstName} ${Provider.of<UserProvider>(context).user!.lastName}',
+            body: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 30.0, vertical: 60.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Text(
+                      widget.bus_number,
                       style: const TextStyle(
-                          color: Color.fromARGB(230, 255, 255, 255),
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold
-                          ),
-                    ),
-                    const Text(
-                      "Please Select your Journey!",
-                      style: TextStyle(
-                          color: Color.fromARGB(150, 255, 255, 255),
-                          fontSize: 20,
-                          ),
-                    ),
-                    const SizedBox(
-                      height: 60,
-                    ),
-                    // dropdown to show the current possible travel places to select the from and to and the dropdown showing the selected from and to
-                    Container(
-                      width: double
-                          .infinity, // This makes the container take the full width.
-                      height: 50.0, // Adjust this value to your desired height.
-                      color: Color(0xff545560), // Adjust this value to your desired background color.
-                      padding: const EdgeInsets.symmetric(
-                          horizontal:
-                              20.0), // Add some padding to make it look nicer.
-                      child: DropdownButtonHideUnderline(
-                        // This removes the default underline of the DropdownButton.
-                        child: DropdownButton<Place>(
-                          isExpanded:
-                              true, // This makes the DropdownButton take the full width of its parent.
-                          hint: from == ''
-                              ? const Text(
-                                  'From',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                )
-                              : Text(
-                                  from,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                          icon: const Icon(Icons.arrow_drop_down),
-                          iconSize: 24,
-                          iconEnabledColor: const Color(0x88ffffff),
-                          dropdownColor: const Color(0xff545560),
-                          elevation: 0,
-                          style: const TextStyle(
-                              color: Color(0x88ffffff),
-                              fontSize: 14,
-                              letterSpacing: 1.2,
-                              fontWeight: FontWeight.w500),
-                          value: null,
-                          items: currentPossibleTravelPlaces
-                              .map((place) => DropdownMenuItem(
-                                    child:  Text(place.name),
-                                    value: place,
-                                  ))
-                              .toList(),
-                          onChanged: (Place? value) {
-                            setState(() {
-                              from = value!.name;
-                            });
-                          },
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      width: double
-                          .infinity, // This makes the container take the full width.
-                      height: 50.0, // Adjust this value to your desired height.
-                      color: Color(0xff545560), // Adjust this value to your desired background color.
-                      padding: const EdgeInsets.symmetric(
-                          horizontal:
-                              20.0), // Add some padding to make it look nicer.
-                      child: DropdownButtonHideUnderline(
-                        // This removes the default underline of the DropdownButton.
-                        child: DropdownButton<Place>(
-                          isExpanded:
-                              true, // This makes the DropdownButton take the full width of its parent.
-                          hint: to == ''
-                              ? const Text(
-                                  'To',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                )
-                              : Text(
-                                  to,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                          icon: const Icon(Icons.arrow_drop_down),
-                          iconSize: 24,
-                          iconEnabledColor: const Color(0x88ffffff),
-                          dropdownColor: const Color(0xff545560),
-                          elevation: 0,
-                          style: const TextStyle(
-                              color: Color(0x88ffffff),
-                              fontSize: 14,
-                              letterSpacing: 1.2,
-                              fontWeight: FontWeight.w500),
-                          value: null,
-                          items: currentPossibleTravelPlaces
-                              .map((place) => DropdownMenuItem(
-                                    child: Text(place.name),
-                                    value: place,
-                                  ))
-                              .toList(),
-                          onChanged: (Place? value) {
-                            setState(() {
-                              to = value!.name;
-                            });
-                          },
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    // button to create the ticket with loading state
-                    GestureDetector(
-                onTap: createTicket,
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xffFFC857),
-                      borderRadius:
-                          BorderRadius.circular(3.0), // Adjust as needed
-                    ),
-                    child: Center(
-                      child: isSubmitting // Replace with your actual condition
-                          ? const SizedBox(
-                              height: 20,
-                              width: 20,
-                              child: CircularProgressIndicator(
-                                color: Color(
-                                  0xff111111,
-                                ),
-                                strokeWidth: 2.0,
-                              ),
-                            )
-                          : const Text(
-                              "Create Ticket",
-                              style: TextStyle(
-                                color: Color(0xff111111),
-                                fontSize: 16,
-                                letterSpacing: 1.5,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
+                          color: Color.fromARGB(200, 255, 255, 255),
+                          fontSize: 30),
                     ),
                   ),
-                ),
-              ),
-                    
-                  ],
-                ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Text(
+                    'Hello, ${Provider.of<UserProvider>(context).user!.firstName} ${Provider.of<UserProvider>(context).user!.lastName}',
+                    style: const TextStyle(
+                        color: Color.fromARGB(230, 255, 255, 255),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const Text(
+                    "Please Select your Journey!",
+                    style: TextStyle(
+                      color: Color.fromARGB(150, 255, 255, 255),
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  // dropdown to show the current possible travel places to select the from and to and the dropdown showing the selected from and to
+                  Container(
+                    width: double
+                        .infinity, // This makes the container take the full width.
+                    height: 50.0, // Adjust this value to your desired height.
+                    color: Color(
+                        0xff545560), // Adjust this value to your desired background color.
+                    padding: const EdgeInsets.symmetric(
+                        horizontal:
+                            20.0), // Add some padding to make it look nicer.
+                    child: DropdownButtonHideUnderline(
+                      // This removes the default underline of the DropdownButton.
+                      child: DropdownButton<Place>(
+                        isExpanded:
+                            true, // This makes the DropdownButton take the full width of its parent.
+                        hint: from == ''
+                            ? const Text(
+                                'From',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              )
+                            : Text(
+                                from,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                        icon: const Icon(Icons.arrow_drop_down),
+                        iconSize: 24,
+                        iconEnabledColor: const Color(0x88ffffff),
+                        dropdownColor: const Color(0xff545560),
+                        elevation: 0,
+                        style: const TextStyle(
+                            color: Color(0x88ffffff),
+                            fontSize: 14,
+                            letterSpacing: 1.2,
+                            fontWeight: FontWeight.w500),
+                        value: null,
+                        items: currentPossibleTravelPlaces
+                            .map((place) => DropdownMenuItem(
+                                  child: Text(place.name),
+                                  value: place,
+                                ))
+                            .toList(),
+                        onChanged: (Place? value) {
+                          setState(() {
+                            from = value!.name;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: double
+                        .infinity, // This makes the container take the full width.
+                    height: 50.0, // Adjust this value to your desired height.
+                    color: Color(
+                        0xff545560), // Adjust this value to your desired background color.
+                    padding: const EdgeInsets.symmetric(
+                        horizontal:
+                            20.0), // Add some padding to make it look nicer.
+                    child: DropdownButtonHideUnderline(
+                      // This removes the default underline of the DropdownButton.
+                      child: DropdownButton<Place>(
+                        isExpanded:
+                            true, // This makes the DropdownButton take the full width of its parent.
+                        hint: to == ''
+                            ? const Text(
+                                'To',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              )
+                            : Text(
+                                to,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                        icon: const Icon(Icons.arrow_drop_down),
+                        iconSize: 24,
+                        iconEnabledColor: const Color(0x88ffffff),
+                        dropdownColor: const Color(0xff545560),
+                        elevation: 0,
+                        style: const TextStyle(
+                            color: Color(0x88ffffff),
+                            fontSize: 14,
+                            letterSpacing: 1.2,
+                            fontWeight: FontWeight.w500),
+                        value: null,
+                        items: currentPossibleTravelPlaces
+                            .map((place) => DropdownMenuItem(
+                                  child: Text(place.name),
+                                  value: place,
+                                ))
+                            .toList(),
+                        onChanged: (Place? value) {
+                          setState(() {
+                            to = value!.name;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  // button to create the ticket with loading state
+                  GestureDetector(
+                    onTap: createTicket,
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xffFFC857),
+                          borderRadius:
+                              BorderRadius.circular(3.0), // Adjust as needed
+                        ),
+                        child: Center(
+                          child:
+                              isSubmitting // Replace with your actual condition
+                                  ? const SizedBox(
+                                      height: 20,
+                                      width: 20,
+                                      child: CircularProgressIndicator(
+                                        color: Color(
+                                          0xff111111,
+                                        ),
+                                        strokeWidth: 2.0,
+                                      ),
+                                    )
+                                  : const Text(
+                                      "Create Ticket",
+                                      style: TextStyle(
+                                        color: Color(0xff111111),
+                                        fontSize: 16,
+                                        letterSpacing: 1.5,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           );
